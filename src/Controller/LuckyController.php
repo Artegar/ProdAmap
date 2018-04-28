ddb3b603d48ec2b6ee40b82f0950e6780ba6038d
artegar <yoann.colin1@gmail.com>
2018-04-28 17:03:37 +0200

    lucky number template and route

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -6,14 +6,22 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 
-class LuckyController
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class LuckyController extends Controller
 {
+	  /**
+    * @Route("/lucky/number")
+    */
 	public function number()
 	{
 		$number = mt_rand(0,100);
 
-		return new Response(
-				'<html><body>Lucky number :'.$number.'</body></html>'
-		);
+		return $this->render('lucky/number.html.twig', array('number'=> $number
+	));
+		
 	}
 }
---
+++ src/Controller/NavigationProdAmapController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class NavigationProdAmapController extends Controller
+{
+    /**
+     * @Route("/navigation/prod/amap", name="navigation_prod_amap")
+     */
+    public function index()
+    {
+        return $this->render('navigation_prod_amap/index.html.twig', [
+            'controller_name' => 'NavigationProdAmapController',
+        ]);
+    }
+}

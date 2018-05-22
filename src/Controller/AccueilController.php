2347c669751e5c740586206a482c43ac2c9698d4
CutyFlam096 <fouquepatrice96@gmail.com>
2018-05-22 20:41:58 +0200

    connection avec user en memoire ok

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -4,11 +4,13 @@ namespace App\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 
 class AccueilController extends Controller
 {
     /**
      * @Route("/accueil", name="accueil")
+     * @Security("has_role('ROLE_ADMIN')")
      */
     public function index()
     {
